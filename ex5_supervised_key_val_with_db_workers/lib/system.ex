3b70124ff477f67db835eb6aab7b4572f3357d83
Arpit Dubey <arpit1294@gmail.com>
2020-02-19 22:09:58 +0000

    ex5 changes

--- ex5_supervised_key_val_with_db_workers/lib/system.ex
+++ ex5_supervised_key_val_with_db_workers/lib/system.ex
@@ -2,6 +2,10 @@ defmodule KeyVal.System do
   use Supervisor
 
   def start do
-    Supervisor.start_link([KeyVal.Manager,KeyVal.DB], strategy: :one_for_one)
+    Supervisor.start_link(__MODULE__,nil)
+  end
+
+  def init(_) do
+      Supervisor.init([KeyVal.Manager,KeyVal.DB], strategy: :one_for_one)
   end
 end

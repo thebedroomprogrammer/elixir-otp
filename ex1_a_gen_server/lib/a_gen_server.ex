175c2e8992fec9b700e5240e268e63b01a007b3a
Arpit Dubey <arpit1294@gmail.com>
2020-02-24 17:58:12 +0000

    readme cleanup

--- ex1_a_gen_server/lib/a_gen_server.ex
+++ ex1_a_gen_server/lib/a_gen_server.ex
@@ -2,35 +2,34 @@ defmodule KeyVal.Server do
   use GenServer
 
   def start do
-    GenServer.start(__MODULE__,nil)
+    GenServer.start(__MODULE__, nil)
   end
 
   def init(_) do
-    {:ok,%{}}
+    {:ok, %{}}
   end
 
-  def put(pid,key,value) do
-    GenServer.cast(pid, {:put,key,value})
+  def put(pid, key, value) do
+    GenServer.cast(pid, {:put, key, value})
   end
 
-  def get(pid,key) do
-    GenServer.call(pid, {:get,key})
+  def get(pid, key) do
+    GenServer.call(pid, {:get, key})
   end
 
-  def del(pid,key) do
-    GenServer.cast(pid, {:del,key})
+  def del(pid, key) do
+    GenServer.cast(pid, {:del, key})
   end
 
-  def handle_call({:get,key},_,state) do
-    {:reply,Map.fetch(state, key),state}
+  def handle_call({:get, key}, _, state) do
+    {:reply, Map.fetch(state, key), state}
   end
 
-  def handle_cast({:put,key,value},state) do
+  def handle_cast({:put, key, value}, state) do
     {:noreply, Map.put(state, key, value)}
   end
 
-  def handle_cast({:del,key},state) do
-    {:noreply,    Map.delete(state, key)}
+  def handle_cast({:del, key}, state) do
+    {:noreply, Map.delete(state, key)}
   end
-
 end
